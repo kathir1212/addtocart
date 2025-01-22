b9bbc45e3cb27c4d505d370ee5603fe5ab638e46
kathir1212 <kathirvel082@gmail.com>
2025-01-22 14:49:32 +0530

    header addicon added

--- src/addtocardmodal.jsx
+++ src/addtocardmodal.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import addicon from './assets/cart-icon-16.png';
+
+
+
 
 
 
@@ -35,15 +39,8 @@ function Addtocardmodal(props) {
   return (
     <>
       
-      <button
-        onClick={toggleModal}
-        className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
-      >
-       Add to cards
-      </button>
+      <img className="w-[10%]" src={addicon} alt="" onClick={toggleModal} />
 
-      
       {isModalOpen && (
         <div
           className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-screen bg-black bg-opacity-50"
--- src/header.jsx
+++ src/header.jsx
@@ -12,7 +12,7 @@ function Header(props) {
       <div className="flex p-6 shadow-lg grid grid-cols-2 gap-2 place-content-evenly ">
         <div><h1 className="text-2xl">logo</h1></div>
         <div><div className=" flex justify-end .. text-2xl">
-        <Addtocardmodal stores = {store}/><sub><span className="bg-green-500 p-1 rounded-3xl">{addLength}</span></sub>
+        <Addtocardmodal stores = {store}/><sup className="mt-[1%]"><span className="bg-green-500 p-1 rounded-[100%]">{addLength}</span></sup>
             </div></div>
       </div>
       </>
